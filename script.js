77fd66ef075a624c10627ab7d190c741aa74e5c5
MaxJGruber <maxjgruber@gmail>
2025-09-10 14:31:21 +0200

    Added toggle theme button with JS

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("JS is connected! 🚀");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+  const toggleBtn = document.getElementById("theme-toggle");
+
+  toggleBtn.addEventListener("click", () => {
+    const currentTheme = body.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    body.setAttribute("data-theme", newTheme);
+  });
+});
